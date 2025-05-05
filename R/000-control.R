03a3388a4fe21ad592fe078e27a31f7bb15d27cc
dougtommet <doug_tommet@brown.edu>
2025-05-05 10:54:18 -0400

    renamed the .R files

--- R/000-control.R
+++ R/000-control.R
@@ -1,4 +1,10 @@
 
-quarto::quarto_render(here::here("R", "000-master.qmd"), output_format = "html")
-fs::file_move(here::here("R", "000-master.html"),
+# Run the data management/log file
+quarto::quarto_render(here::here("R", "_000-master.qmd"), output_format = "html")
+fs::file_move(here::here("R", "_000-master.html"),
               here::here("Reports", stringr::str_c("BASIL RS_", Sys.Date(),".html")))
+
+# Run the 2025-05-02 presentation
+quarto::quarto_render(here::here("R", "ABC_presentation_2025-05-02.qmd"), output_format = "html")
+fs::file_move(here::here("R", "ABC_presentation_2025-05-02.html"),
+              here::here("Reports", stringr::str_c("ABC_presentation_2025-05-02_", Sys.Date(),".html")))
